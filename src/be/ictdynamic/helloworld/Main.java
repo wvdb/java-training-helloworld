ab947d79e1942133764e64b7b8140bb3d05f1aa0
wvdbrande <wim.vandenbrande@one-agency.be>
2017-08-24 10:20:37 +0200

    enhancing vb for interface

--- src/be/ictdynamic/helloworld/Main.java
+++ src/be/ictdynamic/helloworld/Main.java
@@ -5,8 +5,8 @@ import be.ictdynamic.helloworld.oefening_interfaces.DummyInterface;
 public class Main {
 
     public static void main(String[] args) {
-        DummyInterface.DummyInterface1.dummyMethod1("dit is een test");
-        DummyInterface.DummyInterface2.printToUpperCase("dit is nog een test");
+        DummyInterface.InnerInterface1.dummyMethod1("dit is een test");
+        DummyInterface.InnerInterface2.printToUpperCase("dit is nog een test");
     }
 
 }
---
+++ src/be/ictdynamic/helloworld/oefening_interfaces/DummyInterfaceImpl.java
@@ -0,0 +1,11 @@
+package be.ictdynamic.helloworld.oefening_interfaces;
+
+/**
+ * Created by wvdbrand on 24/08/2017.
+ */
+public class DummyInterfaceImpl implements DummyInterface{
+    @Override
+    public String getCompleteName(String firstName, String lastName) {
+        return null;
+    }
+}
