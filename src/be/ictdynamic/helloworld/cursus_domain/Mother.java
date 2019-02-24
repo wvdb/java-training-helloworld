a535f51bb60b70c398dfe5dfbc99fbbe1fa45679
Wvdbrande <wim.vandenbrande@one-agency.be>
2019-02-24 19:29:29 +0100

    Additional exercises.

---
+++ src/be/ictdynamic/helloworld/cursus_domain/Mother.java
@@ -0,0 +1,10 @@
+package be.ictdynamic.helloworld.cursus_domain;
+
+public interface Mother {
+    public default void canMakeNiceDinners() {
+        System.out.println("A successful dinner is one that lasts a while and one where everyone leaves happy.");
+    }
+//    public default void thinkOfClimate() {
+//
+//    }
+}
